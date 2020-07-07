e7a73c5667f75629ead0c1b2457d4fddbf82e78b
web25Lucius <web25Lucius@gmail.com>
2020-07-07 10:58:01 -0600

    mvp

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 
-const Total = props => {
+const Total = () => {
+  const {car: {price}, additionalPrice} = useSelector(state => state);
+
   return (
     <div className="content">
-      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+      <h4>Total Amount: ${price + additionalPrice}</h4>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
+import {rootReducer} from './reducers';
+
+
+import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(rootReducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}>
+  <App />
+</Provider>, rootElement);
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// //import {createStore} from 'redux';
+// import {Provider} from 'react-redux';
+// //import in your reducer
+// import App from './App';
+// import 'bulma/css/bulma.css';
+// import './styles.scss';
+
+// const store = createStore(); //argue in your reducer
+
+// const rootElement = document.getElementById('root');
+// ReactDOM.render(<Provider><App /></Provider>, rootElement);
